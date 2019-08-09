ae26b01d3ee4552f53aa97ef3f0e1de20887cf74
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-08-09 11:14:16 -0500

    Changed app into class component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import FormikRegistrationForm from './components/RegistrationForm';
 
-function App() {
-  return (
-    <div className="App">
-      <FormikRegistrationForm/>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <FormikRegistrationForm/>
+      </div>
+    );
+  };
 }
 
 export default App;
